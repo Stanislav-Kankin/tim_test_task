af4a1a57a1e8395856e2c817696d3a0b0511fb7b
Stanislav.Kankin <stanislav.kankin@gmail.com>
2025-05-29 12:13:38 +0300

    add init.py and users.py in models

---
+++ bot/src/models/__init__.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Настройки подключения к БД
+DATABASE_URL = (
+    f"postgresql+asyncpg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@"
+    f"{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+)
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+    )
+
+Base = declarative_base()
+
+
+async def get_db() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        except Exception as e:
+            await session.rollback()
+            raise e
+        finally:
+            await session.close()
---
+++ bot/src/models/users.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Boolean, BigInteger
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(BigInteger, unique=True, nullable=False)
+    username = Column(String(100))
+    first_name = Column(String(100))
+    last_name = Column(String(100))
+    is_subscribed = Column(Boolean, default=False)
+    is_admin = Column(Boolean, default=False)
+
+    def __repr__(self):
+        return f"User(id={self.id}, username={self.username})"
